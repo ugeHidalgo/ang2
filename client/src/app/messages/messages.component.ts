1e0764610e87a4322c8d3243095c6754af4c9c13
ugeHidalgo <ugeHidalgo@hotmail.com>
2018-02-10 19:09:59 +0100

    Added a count down to clear at clear button in messages component.

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -12,7 +12,26 @@ export class MessagesComponent implements OnInit {
 
   constructor(public messageService: MessageService) { }
 
+  count: number;
+
   ngOnInit() {
+    const me = this;
+
+    me.count = 5;
+    me.countDowmBeforeClearMessages();
+  }
+
+  countDowmBeforeClearMessages(): void {
+    const me = this;
+
+    me.count--;
+    if (me.count <= 0) {
+      me.messageService.clear();
+      return;
+    }
+    setTimeout(() => {
+      me.countDowmBeforeClearMessages();
+    }, 1000);
   }
 
 }
